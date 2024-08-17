b9e4856719c5256349acd155ef56990a972b7c49
Ananya Priyadarshini <143418973+ananyap05@users.noreply.github.com>
2024-08-17 17:26:52 +0530

    Time: 5 ms (57.88%) | Memory: 16.6 MB (29.11%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        //vector<vector<int>> ans=grid;
         int n=grid.size();
         int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m));
         queue<pair<pair<int,int>,int>> q;
-        int vis[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j]=2;
                 }
-                else vis[i][j]=0;
             }
         }
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
         int tm=0;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
@@ -24,9 +24,9 @@ public:
             tm=max(tm,t);
             q.pop();
             for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
                     q.push({{nrow,ncol},t+1});
                     vis[nrow][ncol]=2;
                 }
