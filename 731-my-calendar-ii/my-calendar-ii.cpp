fa2f97fb47e8bd01014e7ce254f6a53d0ae0e141
Ananya Priyadarshini <143418973+ananyap05@users.noreply.github.com>
2024-09-28 13:04:25 +0530

    Time: 164 ms (36.01%) | Memory: 42.7 MB (45.84%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -0,0 +1,34 @@
+class MyCalendarTwo {
+public:
+    map<int, int> eventCount;
+
+    MyCalendarTwo() {}
+
+    bool book(int start, int end) {
+        eventCount[start]++;
+        eventCount[end]--;
+
+        int ongoingEvents = 0;
+        for (auto &[time, change] : eventCount) {
+            ongoingEvents += change;
+            if (ongoingEvents >= 3) {
+                eventCount[start]--;
+                eventCount[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Usage:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool result = obj->book(start, end);
+ */
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
