63accdf46afa893f0128d1b4af24b8778bed7740
Ananya Priyadarshini <143418973+ananyap05@users.noreply.github.com>
2025-01-15 17:02:30 +0530

    Time: 123 ms (58.11%) | Memory: 100.5 MB (25.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool f(int i, int target, vector<vector<int>>& dp,vector<int>& nums){
-        if(target==0) return true;
-        if(i==0) return nums[0]==target;
-        if(dp[i][target]!=-1) return dp[i][target];
-        int notpick=f(i-1,target,dp,nums);
-        int pick=false;
-        if(nums[i]<=target){
-            pick=f(i-1,target-nums[i],dp,nums);
+    bool f(int i, vector<vector<int>>& dp, vector<int>& nums, int sum1){
+        if(sum1==0) return true;
+        if(i==0) return nums[i]==sum1;
+        if(dp[i][sum1]!=-1) return dp[i][sum1];
+        bool notpick=f(i-1,dp,nums,sum1);
+        bool pick=false;
+        if(nums[i]<=sum1){
+            pick=f(i-1,dp,nums,sum1-nums[i]);
         }
-        return dp[i][target]=pick||notpick;
+        return dp[i][sum1]=pick || notpick;
     }
     bool canPartition(vector<int>& nums) {
-        int totSum=0;
         int n=nums.size();
-        totSum=accumulate(nums.begin(),nums.end(),0);
-        if(totSum%2!=0) return false;
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum%2!=0) return false;
         else{
-            int k=totSum/2;
-            vector<vector<int>> dp(n,vector<int>(k+1,-1));
-            return f(n-1,k,dp,nums);
+            int sum1=sum/2;
+            vector<vector<int>> dp(n,vector<int>(sum1+1,-1));
+            return f(n-1,dp,nums,sum1);
         }
+        
     }
 };
